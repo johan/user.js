3002da8930f5b362acdea0b03502427bca13c95e
Johan Sundström <oyasumi@gmail.com>
2012-10-13 17:34:26 -0700

    Support for nested dom specs and template deconstructuring assignments.

--- default.js
+++ default.js
@@ -43,26 +43,65 @@
 
    (see http://goo.gl/ejtMD for a more thorough discussion of something similar)
 
+   The dom prtoperty is recursively defined so you can make nested structures.
+   If you want a property that itself is an object full of matched things, pass
+   an object of sub-dom-spec:s, instead of a string selector:
+
+   on dom:
+        meta:
+          base:  'xpath? /head/base
+          title: 'xpath  string(/head/title)'
+        commits: 'css* li.commit'
+      ready: (dom) ->
+
+   You can also deconstruct repeated templated sections of a page into subarrays
+   scraped as per your specs, by picking a context node for a dom spec. This is
+   done by passing a two-element array: a selector resolving what node/nodes you
+   look at and a dom spec describing how you want it/them deconstructed for you:
+
+   on dom:
+        meta:
+          [ 'xpath /head',
+            base:  'xpath? base
+            title: 'xpath  string(title)'
+          ]
+        commits:
+          [ 'css* li.commit',
+            avatar_url:  ['css img.gravatar', 'xpath string(@src)']
+            author_name: 'xpath string(.//*[@class="author-name"])'
+          ]
+      ready: (dom) ->
+
+   The mandatory/optional selector rules defined above behave as you'd expect as
+   used for context selectors too: a mandatory node or array of nodes will limit
+   what pages your script gets called on to those that match it, so your code is
+   free to assume it will always be there when it runs. An optional context node
+   that is not found will instead result in that part of your DOM being null, or
+   an empty array, in the case of a * selector.
+
  */
+
 function on(opts) {
-  function get(x) { rules[x] = undefined; return opts[x]; }
-  function array(a) { return typeof a === 'object' && a.length ? a : [a]; }
-  function isArray(a) {
-    return '[object Array]' === Object.prototype.toString.call(a);
-  }
+  function get(x)      { rules[x] = undefined; return opts[x]; }
+  function isArray(x)  { return Object_toString.call(x) === '[object Array]'; }
+  function isObject(x) { return Object_toString.call(x) === '[object Object]'; }
+  function array(a)    { return Array_slice.call(a, 0); } // array:ish => Array
+  function arrayify(x) { return isArray(x) ? x : [x]; }  // non-array? => Array
   var input = [] // args for the callback(s?) the script wants to run
-    , rules = Object.create(opts)
+    , rules = Object.create(opts) // wraps opts in a pokeable inherit layer
     , tests =
       { path_re: [test_regexp, location.pathname]
       , query:   [test_query,  location.search]
       , dom:     [test_dom,    document]
       }
     , FAIL = new function() {}
+    , Object_toString = Object.prototype.toString
+    , Array_slice = Array.prototype.slice
     , debug = get('debug')
     , script = get('name')
     , ready = get('ready')
     , load = get('load')
-    , name, rule, test, outcome
+    , name, rule, test, result
     ;
 
   if (typeof ready !== 'function' && typeof load !== 'function')
@@ -74,13 +113,13 @@ function on(opts) {
       if (rule === undefined) continue; // was some callback, or other non-rule
       test = tests[name];
       if (!test) throw new Error('did not grok rule "'+ name +'"!');
-      outcome = run_test(name, rule, test);
-      if (outcome === FAIL) return false; // this page doesn't satisfy all rules
-      if (outcome !== undefined) input.push(outcome);
+      result = test[0].call(test[1], rule);
+      if (result === FAIL) return false; // this page doesn't satisfy all rules
+      if (result !== undefined) input.push(result);
     }
   }
   catch(e) {
-    if (debug) console.warn("on(debug): we didn't run because of", e);
+    if (debug) console.warn("on(debug): we didn't run because " + e.message);
     return false;
   }
 
@@ -90,15 +129,10 @@ function on(opts) {
   });
   return input.concat(opts);
 
-  function run_test(name, rule, test_spec) {
-    var fn = test_spec.shift(), data = test_spec.shift();
-    return fn.apply(data, array(rule));
-  }
-
-  function test_query(rule) {
+  function test_query(spec) {
     var q = unparam(this);
-    if (rule === true) return q; // true => decode the query for me!
-    throw new Error('bad query type '+ (typeof rule) +': '+ rule);
+    if (spec === true) return q; // true => decode the query for me!
+    throw new Error('bad query type '+ (typeof spec) +': '+ rule);
   }
 
   function unparam(query) {
@@ -114,47 +148,84 @@ function on(opts) {
     return data;
   }
 
-  function test_regexp(re, names) {
+  function test_regexp(spec) {
+    if (!isArray(spec)) spec = arrayify(spec);
+    var re = spec.shift();
     if (typeof re === 'string') re = new RegExp(re);
     if (!(re instanceof RegExp))
       throw new Error((typeof re) +' was not a regexp: '+ re);
 
     var ok = re.exec(this);
     if (ok === null) return FAIL;
-    if (!names) return ok;
-    for (var named = {}, i = 1; i < arguments.length; i++)
-      named[arguments[i]] = ok[i];
+    if (!spec.length) return ok;
+    var named = {};
+    ok.shift(); // drop matching-whole-regexp part
+    while (spec.length) named[spec.shift()] = ok.shift();
     return named;
   }
 
-  function test_dom(rules) {
+  // DOM constraint tester / scraper facility:
+  // "this" is the context Node(s) - initially the document
+  // "spec" is either of:
+  //   * css / xpath Selector "selector_type selector"
+  //   * resolved for context [ context Selector, spec ]
+  //   * an Object of spec(s) { property_name: spec, ... }
+  function test_dom(spec) {
     var tests =
-        { css:    not_null($C)
-        , 'css?': $C
-        , 'css+': one_or_more($c)
-        , 'css*': $c
-        , xpath:  not_null($X)
+        { 'css':    not_null($C)
+        , 'css?':   $C
+        , 'css+':   one_or_more($c)
+        , 'css*':   $c
+        , 'xpath':  not_null($X)
         , 'xpath?': $X
         , 'xpath+': one_or_more($x)
         , 'xpath*': $x
         }
-      , predicate = /^((?:css|xpath)[?+*]?)\s+(.*)/
-      , dom = {}
-      , type, rule, func, match, got;
+      , context, results, result, i, property_name;
 
-    for (var name in rules) {
-      if ((match = predicate.exec(rules[name]))) {
+    // validate context:
+    if (this === null || this === FAIL) return this;
+    if (isArray(this)) {
+      for (results = [], i = 0; i < this.length; i++) {
+        result = test_dom.call(this[i], spec);
+        if (result === FAIL) return FAIL;
+        results.push(result);
+      }
+      return results;
+    }
+    if (typeof this !== 'object' || !('nodeType' in this))
+      throw new Error('illegal context: '+ this);
+
+    // handle input spec format:
+    if (typeof spec === 'string') return lookup.call(this, spec);
+    if (isArray(spec)) {
+      context = lookup.call(this, spec[0]);
+      return test_dom.call(context, spec[1]);
+    }
+    if (isObject(spec)) {
+      results = {};
+      for (property_name in spec) {
+        result = test_dom.call(this, spec[property_name]);
+        if (result === FAIL) return FAIL;
+        results[property_name] = result;
+      }
+      return results;
+    }
+
+    throw new Error("dom spec was neither a String, Object nor Array: "+ spec);
+
+    function lookup(rule) {
+      if (typeof rule !== 'string')
+        throw new Error('non-String dom match rule: '+ rule);
+      var match = /^((?:css|xpath)[?+*]?)\s+(.*)/.exec(rule), type, func;
+      if (match) {
         type = match[1];
         rule = match[2];
         func = tests[type];
       }
-      if (!match || !func)
-        throw new Error('unknown dom match rule: '+ rules[name]);
-      if ((got = func.call(this, rule)) === FAIL) return FAIL;
-      dom[name] = got;
-      rules[name] = rule;
+      if (!func) throw new Error('unknown dom match rule: '+ rule);
+      return func.call(this, rule);
     }
-    return dom;
 
     function not_null(fn) { return function(s) {
       var x = fn.apply(this, arguments); return x !== null ? x : FAIL;
@@ -164,7 +235,7 @@ function on(opts) {
       var x = fn.apply(this, arguments); return x.length ? x : FAIL;
     }; }
 
-    function $c(css) { return this.querySelectorAll(css); }
+    function $c(css) { return array(this.querySelectorAll(css)); }
     function $C(css) { return this.querySelector(css); }
 
     function $x(xpath) {
