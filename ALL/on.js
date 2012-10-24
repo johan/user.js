1948aa43300e4b206ebdde6b17b3130abc9fbecb
Johan Sundström <oyasumi@gmail.com>
2012-10-24 01:02:27 -0700

    Expose on.dom, on.query and on.path_re methods after first call.

--- ALL/on.js
+++ ALL/on.js
@@ -82,21 +82,43 @@
  */
 
 function on(opts) {
-  function get(x)      { rules[x] = undefined; return opts[x]; }
-  function isArray(x)  { return Object_toString.call(x) === '[object Array]'; }
-  function isObject(x) { return Object_toString.call(x) === '[object Object]'; }
-  function array(a)    { return Array_slice.call(a, 0); } // array:ish => Array
-  function arrayify(x) { return isArray(x) ? x : [x]; }  // non-array? => Array
-  var input = [] // args for the callback(s?) the script wants to run
-    , rules = Object.create(opts) // wraps opts in a pokeable inherit layer
-    , tests =
-      { path_re: [test_regexp, location.pathname]
-      , query:   [test_query,  location.search]
-      , dom:     [test_dom,    document]
-      }
-    , FAIL = new function() {}
-    , Object_toString = Object.prototype.toString
+  var Object_toString = Object.prototype.toString
     , Array_slice = Array.prototype.slice
+    , FAIL = on.FAIL || (function() {
+        var tests =
+              { path_re: { fn: test_regexp, this: location.pathname }
+              , query:   { fn: test_query,  this: location.search }
+              , dom:     { fn: test_dom,    this: document
+                         , my: { 'css':    not_null($C)
+                               , 'css?':   $C
+                               , 'css+':   one_or_more($c)
+                               , 'css*':   $c
+                               , 'xpath':  not_null($X)
+                               , 'xpath?': $X
+                               , 'xpath+': one_or_more($x)
+                               , 'xpath*': $x
+                               }
+                         }
+              }
+          , name, test, me, my, mine
+          ;
+
+        for (name in tests) {
+          test = tests[name];
+          me = test.fn;
+          if ((my = test.my))
+            for (mine in my)
+              me[mine] = my[mine];
+          on[name] = me.bind(test.this);
+        }
+
+        return on.FAIL = new function() {
+          this.toString = this.valueOf = function() { return 'FAIL'; };
+        };
+      })()
+
+    , input = [] // args for the callback(s?) the script wants to run
+    , rules = Object.create(opts) // wraps opts in a pokeable inherit layer
     , debug = get('debug')
     , script = get('name')
     , ready = get('ready')
@@ -110,11 +132,11 @@ function on(opts) {
   try {
     for (name in rules) {
       rule = rules[name];
-      if (rule === undefined) continue; // was some callback, or other non-rule
-      test = tests[name];
+      if (rule === undefined) continue; // was some callback or other non-rule
+      test = on[name];
       if (!test) throw new Error('did not grok rule "'+ name +'"!');
-      result = test[0].call(test[1], rule);
-      if (result === FAIL) return false; // this page doesn't satisfy all rules
+      result = test(rule);
+      if (result === FAIL) return false; // the page doesn't satisfy all rules
       if (result !== undefined) input.push(result);
     }
   }
@@ -129,10 +151,16 @@ function on(opts) {
   });
   return input.concat(opts);
 
+  function get(x) { rules[x] = undefined; return opts[x]; }
+  function isArray(x)  { return Object_toString.call(x) === '[object Array]'; }
+  function isObject(x) { return Object_toString.call(x) === '[object Object]'; }
+  function array(a)    { return Array_slice.call(a, 0); } // array:ish => Array
+  function arrayify(x) { return isArray(x) ? x : [x]; }  // non-array? => Array
+
   function test_query(spec) {
     var q = unparam(this);
-    if (spec === true) return q; // true => decode the query for me!
-    throw new Error('bad query type '+ (typeof spec) +': '+ rule);
+    if (spec === true || spec == null) return q; // decode the query for me!
+    throw new Error('bad query type '+ (typeof spec) +': '+ spec);
   }
 
   function unparam(query) {
@@ -164,56 +192,39 @@ function on(opts) {
     return named;
   }
 
+  function not_null(fn) { return function(s) {
+    var x = fn.apply(this, arguments); return x !== null ? x : FAIL;
+  }; }
+
+  function one_or_more(fn) { return function(s) {
+    var x = fn.apply(this, arguments); return x.length ? x : FAIL;
+  }; }
+
+  function $c(css) { return array(this.querySelectorAll(css)); }
+  function $C(css) { return this.querySelector(css); }
+
+  function $x(xpath) {
+    var doc = this.evaluate ? this : this.ownerDocument, next;
+    var got = doc.evaluate(xpath, this, null, 0, null), all = [];
+    switch (got.resultType) {
+      case got.STRING_TYPE:  return got.stringValue;
+      case got.NUMBER_TYPE:  return got.numberValue;
+      case got.BOOLEAN_TYPE: return got.booleanValue;
+      default: while ((next = got.iterateNext())) all.push(next); return all;
+    }
+  }
+  function $X(xpath) {
+    var got = $x.call(this, xpath);
+    return got instanceof Array ? got[0] || null : got;
+  }
+
   // DOM constraint tester / scraper facility:
   // "this" is the context Node(s) - initially the document
   // "spec" is either of:
   //   * css / xpath Selector "selector_type selector"
   //   * resolved for context [ context Selector, spec ]
   //   * an Object of spec(s) { property_name: spec, ... }
-  function test_dom(spec) {
-    var tests =
-        { 'css':    not_null($C)
-        , 'css?':   $C
-        , 'css+':   one_or_more($c)
-        , 'css*':   $c
-        , 'xpath':  not_null($X)
-        , 'xpath?': $X
-        , 'xpath+': one_or_more($x)
-        , 'xpath*': $x
-        }
-      , context, results, result, i, property_name;
-
-    // validate context:
-    if (this === null || this === FAIL) return this;
-    if (isArray(this)) {
-      for (results = [], i = 0; i < this.length; i++) {
-        result = test_dom.call(this[i], spec);
-        if (result === FAIL) return FAIL;
-        results.push(result);
-      }
-      return results;
-    }
-    if (typeof this !== 'object' || !('nodeType' in this))
-      throw new Error('illegal context: '+ this);
-
-    // handle input spec format:
-    if (typeof spec === 'string') return lookup.call(this, spec);
-    if (isArray(spec)) {
-      context = lookup.call(this, spec[0]);
-      return test_dom.call(context, spec[1]);
-    }
-    if (isObject(spec)) {
-      results = {};
-      for (property_name in spec) {
-        result = test_dom.call(this, spec[property_name]);
-        if (result === FAIL) return FAIL;
-        results[property_name] = result;
-      }
-      return results;
-    }
-
-    throw new Error("dom spec was neither a String, Object nor Array: "+ spec);
-
+  function test_dom(spec, context) {
     function lookup(rule) {
       if (typeof rule !== 'string')
         throw new Error('non-String dom match rule: '+ rule);
@@ -221,36 +232,44 @@ function on(opts) {
       if (match) {
         type = match[1];
         rule = match[2];
-        func = tests[type];
+        func = test_dom[type];
       }
-      if (!func) throw new Error('unknown dom match rule: '+ rule);
+      if (!func) throw new Error('unknown dom match rule '+ type +': '+ rule);
       return func.call(this, rule);
     }
 
-    function not_null(fn) { return function(s) {
-      var x = fn.apply(this, arguments); return x !== null ? x : FAIL;
-    }; }
+    var results, result, i, property_name;
+    if (context === undefined) context = this;
 
-    function one_or_more(fn) { return function(s) {
-      var x = fn.apply(this, arguments); return x.length ? x : FAIL;
-    }; }
-
-    function $c(css) { return array(this.querySelectorAll(css)); }
-    function $C(css) { return this.querySelector(css); }
-
-    function $x(xpath) {
-      var doc = this.evaluate ? this : this.ownerDocument, next;
-      var got = doc.evaluate(xpath, this, null, 0, null), all = [];
-      switch (got.resultType) {
-        case got.STRING_TYPE:  return got.stringValue;
-        case got.NUMBER_TYPE:  return got.numberValue;
-        case got.BOOLEAN_TYPE: return got.booleanValue;
-        default: while ((next = got.iterateNext())) all.push(next); return all;
+    // validate context:
+    if (context === null || context === FAIL) return context;
+    if (isArray(context)) {
+      for (results = [], i = 0; i < context.length; i++) {
+        result = test_dom.call(context[i], spec);
+        if (result === FAIL) return FAIL;
+        results.push(result);
       }
+      return results;
     }
-    function $X(xpath) {
-      var got = $x.call(this, xpath);
-      return got instanceof Array ? got[0] || null : got;
+    if (typeof context !== 'object' || !('nodeType' in context))
+      throw new Error('illegal context: '+ context);
+
+    // handle input spec format:
+    if (typeof spec === 'string') return lookup.call(context, spec);
+    if (isArray(spec)) {
+      context = lookup.call(context, spec[0]);
+      return test_dom.call(context, spec[1]);
     }
+    if (isObject(spec)) {
+      results = {};
+      for (property_name in spec) {
+        result = test_dom.call(context, spec[property_name]);
+        if (result === FAIL) return FAIL;
+        results[property_name] = result;
+      }
+      return results;
+    }
+
+    throw new Error("dom spec was neither a String, Object nor Array: "+ spec);
   }
 }
